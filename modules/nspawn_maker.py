3ee098f980ea4d05e5fb802c5f671b20b687a228
Gel0bmstu <leshikne@gmail.com>
2020-06-10 03:41:49 +0300

    # machine_check fix

--- modules/nspawn_maker.py
+++ modules/nspawn_maker.py
@@ -45,10 +45,13 @@ class NspawnMaker:
         return repo_file.group(0)
         
     def check_machine_exist_(self, machine):
-        output = subprocess.check_output(['/usr/bin/sudo', '/usr/bin/rm', '-rf', os.path.realpath('.') + '/*.rpm'])
+        output = subprocess.check_output(['/usr/bin/sudo', '/usr/bin/machinectl'])
         output = output.decode('utf-8')
 
-        machines = s.split('\n')[1:-2]
+        if output == 'No machines.':
+            return False
+
+        machines = output.split('\n')[1:-2]
 
         for machine_name in machines:
             if machine_name.split()[0] == machine:
@@ -58,31 +61,33 @@ class NspawnMaker:
 
     def make_container(self):
         self.log_.l('Making nspawn container ...')
-        repo_pkg = self.find_repos_(self.release_, self.arch_)
-        pkgs = 'NetworkManager less systemd-units openssh-server vim systemd procps-ng timezone dnf sudo usbutils passwd basesystem-minimal rosa-repos-keys rosa-repos'
-        self.log_.l('Making chroot in {}'.format(self.rootfs_dir_))
-        if os.path.exists(self.rootfs_dir_):
-            if os.path.ismount(self.rootfs_dir_):
-                self.log_.l('Directory in mounted, unmount')
-                subprocess.check_output(['/usr/bin/sudo', '/bin/umount', self.cache_dir_])
-                subprocess.check_output(['/usr/bin/sudo', '/bin/umount', self.boot_dir_])
-                subprocess.check_output(['/usr/bin/sudo', '/bin/umount', self.rootfs_dir_])
-            subprocess.check_output(['/usr/bin/sudo', 'rm', '-rf', self.rootfs_dir_])
-        subprocess.check_output(['/usr/bin/sudo', 'rpm', '-Uvh', '--ignorearch', '--nodeps', repo_pkg, '--root', self.rootfs_dir_])
-        subprocess.check_output(['/usr/bin/sudo', 'dnf', '-y', 'install', '--nogpgcheck', '--installroot=' + self.rootfs_dir_, \
-            '--releasever=' + self.release_, '--forcearch=' + self.arch_] + pkgs.split())
-        # copy fstab
-        subprocess.check_output(['/usr/bin/sudo', 'cp', '-fv', 'fstab.template', self.rootfs_dir_ + '/etc/fstab'])
-        # perl -e 'print crypt($ARGV[0], "password")' omv
-        subprocess.check_output(['/usr/bin/sudo', 'useradd', '--prefix', self.rootfs_dir_, 'rosa', '-p', 'pabc4KTyGYBtg', '-G', 'wheel', '-m'])
-        subprocess.check_output(['/usr/bin/sudo', '/usr/bin/mkdir', self.rootfs_dir_ + '/etc/systemd/system/console-getty.service.d/'])
-        subprocess.check_output(['/usr/bin/sudo', '/usr/bin/touch', self.rootfs_dir_ + '/etc/systemd/system/console-getty.service.d/override.conf'])
-        subprocess.check_output(['/usr/bin/sudo', 'chmod', '666', self.rootfs_dir_ + '/etc/systemd/system/console-getty.service.d/override.conf'])
-        f = open(self.rootfs_dir_ + '/etc/systemd/system/console-getty.service.d/override.conf', 'w+')
-        f.write(self.autologin_service_)
+        # repo_pkg = self.find_repos_(self.release_, self.arch_)
+        # pkgs = 'NetworkManager less systemd-units openssh-server vim systemd procps-ng timezone dnf sudo usbutils passwd basesystem-minimal rosa-repos-keys rosa-repos'
+        # self.log_.l('Making chroot in {}'.format(self.rootfs_dir_))
+        # if os.path.exists(self.rootfs_dir_):
+        #     if os.path.ismount(self.rootfs_dir_):
+        #         self.log_.l('Directory in mounted, unmount')
+        #         subprocess.check_output(['/usr/bin/sudo', '/bin/umount', self.cache_dir_])
+        #         subprocess.check_output(['/usr/bin/sudo', '/bin/umount', self.boot_dir_])
+        #         subprocess.check_output(['/usr/bin/sudo', '/bin/umount', self.rootfs_dir_])
+        #     subprocess.check_output(['/usr/bin/sudo', 'rm', '-rf', self.rootfs_dir_])
+        # subprocess.check_output(['/usr/bin/sudo', 'rpm', '-Uvh', '--ignorearch', '--nodeps', repo_pkg, '--root', self.rootfs_dir_])
+        # subprocess.check_output(['/usr/bin/sudo', 'dnf', '-y', 'install', '--nogpgcheck', '--installroot=' + self.rootfs_dir_, \
+        #     '--releasever=' + self.release_, '--forcearch=' + self.arch_] + pkgs.split())
+        # # copy fstab
+        # subprocess.check_output(['/usr/bin/sudo', 'cp', '-fv', 'fstab.template', self.rootfs_dir_ + '/etc/fstab'])
+        # # perl -e 'print crypt($ARGV[0], "password")' omv
+        # subprocess.check_output(['/usr/bin/sudo', 'useradd', '--prefix', self.rootfs_dir_, 'rosa', '-p', 'pabc4KTyGYBtg', '-G', 'wheel', '-m'])
+        # subprocess.check_output(['/usr/bin/sudo', '/usr/bin/mkdir', self.rootfs_dir_ + '/etc/systemd/system/console-getty.service.d/'])
+        # subprocess.check_output(['/usr/bin/sudo', '/usr/bin/touch', self.rootfs_dir_ + '/etc/systemd/system/console-getty.service.d/override.conf'])
+        # subprocess.check_output(['/usr/bin/sudo', 'chmod', '666', self.rootfs_dir_ + '/etc/systemd/system/console-getty.service.d/override.conf'])
+        # f = open(self.rootfs_dir_ + '/etc/systemd/system/console-getty.service.d/override.conf', 'w+')
+        # f.write(self.autologin_service_)
 
         if self.check_machine_exist_(self.machine_name_):
+            self.log_.l('Machine {} exists. Interrupting its work.')
             subprocess.check_output(['/usr/bin/sudo', '/usr/bin/machinectl', 'terminate', self.machine_name_])
+            time.sleep(3)
 
         devnull = open('/dev/null', "w")
         p = subprocess.Popen(['/usr/bin/sudo', 'systemd-nspawn', '-bD', self.rootfs_dir_, '-M', self.machine_name_], stdout=devnull)
